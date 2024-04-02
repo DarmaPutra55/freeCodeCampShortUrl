67a911cd42701cb8ec5695dcfeb29f59827fb2c6
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2024-04-02 11:41:59 +0800

    Make validation strictier

--- index.mjs
+++ index.mjs
@@ -30,71 +30,90 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/shorturl/:short_url", async function (req, res) {
   const short_url = req.params.short_url;
-  try {
-    const url = await prisma.shortUrl.findFirstOrThrow({
+  prisma.shortUrl
+    .findFirstOrThrow({
       where: {
         shorten_url: short_url,
       },
+    })
+    .then(function (url) {
+      return res.redirect(url.original_url);
+    })
+    .catch(function (err) {
+      return res.json({ error: "invalid url" });
     });
-    return res.redirect(url.original_url);
-  } catch (err) {
-    return res.json({ error: "invalid url" });
-  }
 });
 
 app.post("/api/shorturl", function (req, res) {
-  const url = req.body.url;
-  const host = req.body.url.split("//")[1].split("/")[0];
-
-  lookup(host, async (err) => {
-    if (err) {
-      console.log(err);
-      res.json({ error: "invalid url" });
-    } else {
-      let val;
-      let short_url;
-      const url_count = await prisma.shortUrl.count({
-        where: {
-          original_url: url,
-        },
-      });
-      if (url_count > 0) {
-        short_url = await prisma.shortUrl.findFirst({
-          where: {
-            original_url: url,
-          },
-        });
-        res.json({
-          original_url: short_url.original_url,
-          short_url: short_url.shorten_url,
-        });
-      }
-      while (true) {
-        val = (
-          Math.floor(Math.random() * (9999999999 - 1 + 1)) + 9999999999
-        ).toString();
-        const short_url_count = await prisma.shortUrl.count({
-          where: {
-            shorten_url: val,
-          },
-        });
-        if (short_url_count === 0) {
-          break;
+  try {
+    const url = req.body.url;
+    const host = req.body.url.split("//")[1].split("/")[0];
+    lookup(host, async function (err) {
+      if (err) {
+        res.json({ error: "invalid url" });
+      } else {
+        let val;
+        let short_url;
+        let url_count = 0;
+        prisma.shortUrl
+          .count({
+            where: {
+              original_url: url,
+            },
+          })
+          .then(function (res) {
+            url_count = res;
+          });
+        if (url_count > 0) {
+          prisma.shortUrl
+            .findFirst({
+              where: {
+                original_url: url,
+              },
+            })
+            .then(function (short_url) {
+              res.json({
+                original_url: short_url.original_url,
+                short_url: short_url.shorten_url,
+              });
+            });
         }
+        while (true) {
+          val = (
+            Math.floor(Math.random() * (9999999999 - 1 + 1)) + 9999999999
+          ).toString();
+          let short_url_count = 0;
+          prisma.shortUrl
+            .count({
+              where: {
+                shorten_url: val,
+              },
+            })
+            .then(function (count) {
+              short_url_count = count;
+            });
+          if (short_url_count === 0) {
+            break;
+          }
+        }
+        prisma.shortUrl
+          .create({
+            data: {
+              shorten_url: val,
+              original_url: url,
+            },
+          })
+          .then(function (short_url) {
+            res.json({
+              original_url: url,
+              short_url: short_url.shorten_url,
+            });
+          });
       }
-      short_url = await prisma.shortUrl.create({
-        data: {
-          shorten_url: val,
-          original_url: url,
-        },
-      });
-
-      res.json({
-        original_url: url,
-        short_url: short_url.shorten_url,
-      });
-    }
-  });
+    });
+  } catch (err) {
+    res.json({ error: "invalid url" });
+  }
 });
 
 app.listen(port, function () {
